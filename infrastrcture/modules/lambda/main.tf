1a41983c560b8b6ece2189c2c3b804347ddad729
Kristiyandz <kristiyandzhakov@gmail.com>
2022-12-24 15:44:51 +0000

    fix function name

--- infrastrcture/modules/lambda/main.tf
+++ infrastrcture/modules/lambda/main.tf
@@ -1,7 +1,7 @@
 data "archive_file" "my_test_lambda_archive" {
   type        = "zip"
-  source_file = "./python/testLambda.ts"
-  output_path = "lambda/dist/testLambda.zip"
+  source_file = "./python/test_lambda.py"
+  output_path = "lambda/dist/test_lambda.zip"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
@@ -24,7 +24,7 @@ resource "aws_lambda_function" "my_test_lambda" {
   function_name    = var.function_name
   filename         = data.archive_file.my_test_lambda_archive.output_path
   role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "testLambda.handler"
+  handler          = "test_lambda.handler"
   source_code_hash = filebase64sha256(data.archive_file.my_test_lambda_archive.output_path)
 
   # Lambda Runtimes can be found here: https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html
