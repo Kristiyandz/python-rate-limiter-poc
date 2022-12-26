0c528e8989b6300b436045b196dadae2d882c928
Kristiyan Dzhakov <kristiyandzhakov@gmail.com>
2022-12-26 18:36:44 +0000

    remove unused variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,12 +3,3 @@ variable "function_name" {
   type        = string
   default     = "python_test_lambda"
 }
-
-variable "aws_region" {
-  type = string
-}
-
-variable "aws_account_id" {
-  type = string
-}
-
