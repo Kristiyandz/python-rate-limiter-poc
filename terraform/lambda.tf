fb1099e8c3af1facf5c775c2a4aafbde1d7007c0
Kristiyan Dzhakov <kristiyandzhakov@gmail.com>
2022-12-26 18:31:24 +0000

    remove variables and hardcode info

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,6 +1,3 @@
-locals {
-  aws_region = "eu-west-2"
-}
 
 data "archive_file" "my_test_lambda_archive" {
   type        = "zip"
@@ -50,6 +47,6 @@ resource "aws_lambda_permission" "apigw_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:${aws_api_gateway_rest_api.MyDemoAPI.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn = "arn:aws:execute-api:eu-west-2:580549185469:${aws_api_gateway_rest_api.MyDemoAPI.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
