abfbc325eb7c96c69aeaecfb2018af5491becc7c
Kristiyandz <kristiyandzhakov@gmail.com>
2022-12-27 14:13:58 +0000

    fix naming

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -25,7 +25,7 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = aws_lambda_function.my_test_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_usage_plan" "MyUsagePlan" {
+resource "aws_api_gateway_usage_plan" "myusageplan" {
   name        = "my-usage-plan"
   description = "my description"
 
