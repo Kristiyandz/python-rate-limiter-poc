804492d8a69d298539b3c1547728d5b344f11f75
Kristiyandz <kristiyandzhakov@gmail.com>
2022-12-27 14:54:42 +0000

    update terraform

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -22,6 +22,12 @@ resource "aws_api_gateway_rest_api" "example" {
   name = "example"
 }
 
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "dev"
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.example.id
+}
+
 resource "aws_api_gateway_deployment" "example" {
   rest_api_id = aws_api_gateway_rest_api.example.id
 
