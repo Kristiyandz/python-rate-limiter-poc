39e55a2b3401a935079e77ba440aff5cf9b3b4f5
Kristiyandz <kristiyandzhakov@gmail.com>
2022-12-27 14:50:30 +0000

    update api gtw terraform resources

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,7 +1,4 @@
-resource "aws_api_gateway_rest_api" "MyDemoAPI" {
-  name        = "MyDemoAPI"
-  description = "This is my API for demonstration purposes"
-
+resource "aws_api_gateway_rest_api" "example" {
   body = jsonencode({
     openapi = "3.0.1"
     info = {
@@ -22,13 +19,14 @@ resource "aws_api_gateway_rest_api" "MyDemoAPI" {
     }
   })
 
+  name = "example"
 }
 
-resource "aws_api_gateway_deployment" "dev" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
 
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.MyDemoAPI.body))
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.example.body))
   }
 
   lifecycle {
@@ -36,45 +34,35 @@ resource "aws_api_gateway_deployment" "dev" {
   }
 }
 
-resource "aws_api_gateway_stage" "dev" {
-  deployment_id = aws_api_gateway_deployment.dev.id
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  stage_name    = "dev"
+resource "aws_api_gateway_stage" "development" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  stage_name    = "development"
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "resource"
-  parent_id   = aws_api_gateway_rest_api.MyDemoAPI.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+resource "aws_api_gateway_stage" "production" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  stage_name    = "production"
 }
 
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.my_test_lambda.invoke_arn
-}
-
-resource "aws_api_gateway_usage_plan" "myusageplan" {
-  name        = "my-usage-plan"
-  description = "my description"
+resource "aws_api_gateway_usage_plan" "example" {
+  name         = "my-usage-plan"
+  description  = "my description"
+  product_code = "MYCODE"
 
   api_stages {
-    api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-    stage  = aws_api_gateway_stage.dev.stage_name
+    api_id = aws_api_gateway_rest_api.example.id
+    stage  = aws_api_gateway_stage.development.stage_name
+  }
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.example.id
+    stage  = aws_api_gateway_stage.production.stage_name
   }
 
   quota_settings {
-    limit  = 5
+    limit  = 3
     offset = 0
     period = "DAY"
   }
@@ -83,14 +71,4 @@ resource "aws_api_gateway_usage_plan" "myusageplan" {
     burst_limit = 5
     rate_limit  = 5
   }
-}
-
-resource "aws_api_gateway_api_key" "mykey" {
-  name = "python_rate_limit_key"
-}
-
-resource "aws_api_gateway_usage_plan_key" "main" {
-  key_id        = aws_api_gateway_api_key.mykey.id
-  key_type      = "API_KEY"
-  usage_plan_id = aws_api_gateway_usage_plan.myusageplan.id
 }
